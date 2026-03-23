0cdc0e546cf4742e0f7ecf2a36bc9051c0951785
webenquiry11-svg <webenquiry11@gmail.com>
2026-03-23 13:24:33 +0530

    add GSt verification and make admin dashboard

--- client/src/app/Footer/page.tsx
+++ client/src/app/Footer/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import Link from 'next/link';
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaPaperPlane, FaEnvelope, FaPhone, FaMapMarkerAlt } from 'react-icons/fa';
