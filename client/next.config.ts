608af403c0567a6fed6909de4631e1afc4e71a51
webenquiry11-svg <webenquiry11@gmail.com>
2026-04-10 12:02:55 +0530

    footer link working

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:5000/api/:path*', // Proxy to Node.js backend
+      },
+    ];
   },
 };
 export default nextConfig;
